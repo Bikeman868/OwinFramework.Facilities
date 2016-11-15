656c6b0eae5cb23f06c2eb7ed44c7b4e06297c26
Martin Halliday <martin.bikeman@gmail.com>
2016-11-14 22:52:00 -0800

    Started testing identity store facility

--- UnitTests/CacheTokenStoreFacilityTests.cs
+++ UnitTests/CacheTokenStoreFacilityTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OwinFramework.Facilities.TokenStore.Cache;
 using OwinFramework.Interfaces.Builder;
 using OwinFramework.InterfacesV1.Facilities;
---
+++ UnitTests/MockConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Moq.Modules;
+using OwinFramework.Interfaces.Builder;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// This mock of IConfiguration supplies default values for all configuration
+    /// </summary>
+    public class MockConfiguration: ConcreteImplementationProvider<IConfiguration>, IConfiguration, IDisposable
+    {
+        protected override IConfiguration GetImplementation(IMockProducer mockProducer)
+        {
+            return this;
+        }
+
+        public IDisposable Register<T>(string path, Action<T> onChangeAction, T defaultValue)
+        {
+            onChangeAction(defaultValue);
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
