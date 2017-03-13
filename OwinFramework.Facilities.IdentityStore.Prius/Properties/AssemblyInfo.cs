1bacd2436ff1d63f06a2328768a3a5ff8a4bd626
Martin Halliday <martin.bikeman@gmail.com>
2017-03-12 22:06:02 -0700

    Released to NuGet

--- OwinFramework.Facilities.IdentityStore.Prius/Properties/AssemblyInfo.cs
+++ OwinFramework.Facilities.IdentityStore.Prius/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.5")]
-[assembly: AssemblyFileVersion("2.2.5")]
+[assembly: AssemblyVersion("2.2.6")]
+[assembly: AssemblyFileVersion("2.2.6")]
 
 [assembly: InternalsVisibleTo("UnitTests")]
