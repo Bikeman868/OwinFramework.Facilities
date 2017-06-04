b4b099475650c84adb74b8388a5c7c32b69b613f
Martin Halliday <martin.bikeman@gmail.com>
2017-06-04 16:44:23 -0700

    Released Prius identity store to NuGet

--- NuGet/Package.cmd
+++ NuGet/Package.cmd
@@ -1,3 +1,4 @@
 nuget.exe pack Owin.Framework.Facilities.TokenStore.Cache.nuspec
 nuget.exe pack Owin.Framework.Facilities.Cache.Local.nuspec
 nuget.exe pack Owin.Framework.Facilities.IdentityStore.Prius.nuspec
+nuget.exe pack Owin.Framework.Facilities.TokenStore.Prius.nuspec
--- OwinFramework.Facilities.TokenStore.Prius/Properties/AssemblyInfo.cs
+++ OwinFramework.Facilities.TokenStore.Prius/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("OwinFramework.Facilities.TokenStore.Cache")]
+[assembly: AssemblyTitle("OwinFramework.Facilities.TokenStore.Prius")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OwinFramework.Facilities.TokenStore.Cache")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("OwinFramework.Facilities.TokenStore.Prius")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.4")]
-[assembly: AssemblyFileVersion("2.2.4")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
 [assembly: InternalsVisibleTo("UnitTests")]
