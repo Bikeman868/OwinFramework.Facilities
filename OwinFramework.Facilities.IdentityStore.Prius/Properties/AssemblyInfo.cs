f5b5a5e7afc99fdf581ab36e996e6b1395efc3ed
Martin Halliday <martin.bikeman@gmail.com>
2017-06-24 22:14:19 -0700

    Updated to Owin Framework 2.5.1

--- OwinFramework.Facilities.IdentityStore.Prius/Properties/AssemblyInfo.cs
+++ OwinFramework.Facilities.IdentityStore.Prius/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("2.5.0")]
+[assembly: AssemblyVersion("2.5.1")]
+[assembly: AssemblyFileVersion("2.5.1")]
 
 [assembly: InternalsVisibleTo("UnitTests")]
